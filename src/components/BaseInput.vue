6a24fa7600445a98049fa7616af89d3e088bf1a3
Peter Dinis <pdinis1@gmail.com>
2020-01-10 22:09:19 +0100

    first-commit

---
+++ src/components/BaseInput.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <input 
+            type="text"
+            class="input"
+            :value="value"
+            v-on="listeners"
+        />
+    </div>
+</template>
+
+<script>
+export default {
+    // props definition
+    props: {
+        value: {
+            type: String,
+            default: "",
+        }
+    },
+
+    computed: {
+        listeners() {
+            return {
+               // Pass all component listeners directly to input 
+                ...this.$listeners,
+                input: event => this.$emit('input', event.target.value)
+            }
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .input {
+        width: 100%;
+        padding: 8px 10px;
+        border: 1px solid #32485F;
+    }
+</style>
